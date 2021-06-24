272f519668eb34ce586f9ea8c6a89c58f3aa20f4
Abhishek Mishra <abmishra700@gmail.com>
2021-06-24 16:11:46 +0530

    server create and test with routhing and status code

--- files.js
+++ files.js
@@ -1,46 +1,46 @@
 const fs = require("fs");
 
-// ////reading files////
-// fs.readFile("./docs/blog.txt", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(data.toString());
-// });
+////reading files////
+fs.readFile("./docs/blog.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
 
 // console.log("last line");
 
 // // // writing files // // //
 
-// fs.writeFile('./docs/blog.txt','hello Namaste',()=>{
-//    console.log('file is change');
-// })
+fs.writeFile('./docs/blog.txt','hello Namaste',()=>{
+   console.log('file is change');
+})
 
 // //// Directories  ////
 
-// if (!fs.existsSync("./assets")) {
-//   fs.mkdir("./assets", (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log("folder created");
-//   });
-// } else {
-//   fs.rmdir("./assets", (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log("folder deleted");
-//   });
-// }
+if (!fs.existsSync("./assets")) {
+  fs.mkdir("./assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder created");
+  });
+} else {
+  fs.rmdir("./assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder deleted");
+  });
+}
 
 // / Delete  ///
 
-// if(fs.existsSync('./docs/delete.txt')){
-//    fs.unlink('./docs/delete.txt',(err)=>{
-//       if(err){
-//          console.log(err);
-//       }
-//       console.log('file Deleted');
-//    })
-// }
+if(fs.existsSync('./docs/delete.txt')){
+   fs.unlink('./docs/delete.txt',(err)=>{
+      if(err){
+         console.log(err);
+      }
+      console.log('file Deleted');
+   })
+}
